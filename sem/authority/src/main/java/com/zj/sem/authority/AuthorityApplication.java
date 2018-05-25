cc59634d2b2ad899ddeec0e57da136a58b4bae7e
zj.dalian <18604265099@126.com>
2018-05-25 18:06:27 +0800

    Initial commit

---
+++ sem/authority/src/main/java/com/zj/sem/authority/AuthorityApplication.java
@@ -0,0 +1,12 @@
+package com.zj.sem.authority;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorityApplication.class, args);
+    }
+}
